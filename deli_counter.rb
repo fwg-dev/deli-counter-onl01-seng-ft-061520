1a7b5c7e869f0b3d2bf9d30886e61566f8d03e91
Faith  Gicheha <fwgicheh@gmail.com>
2020-06-22 22:07:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,47 +1,24 @@
 
-def line(array)
-  if array.length >= 1
-    nuarray = []
-    counter = 1
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1
-    end
-    puts "The line is currently: #{nuarray.join(" ")}"
+katz_deli = []
+def line (array)
+  if array.count == 0
+     puts "The line is currently empty."
   else
-    puts "The line is currently empty."
+     response = "The line is currently:"
+     array.each_with_index do |name, index|
+        response += " #{index+1}. #{name}"
+  end
+  puts response
   end
 end
-
-line(katz_deli)
-
-def line_simple(array)
-  current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|
-
-    current_line << " #{indexemus}. #{value},"
-
-  end
-  puts current_line
+def take_a_number (array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
-
-line_simple(katz_deli)
-
-def take_a_number(line, new_person)
-  line.push(new_person)
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
-end
-
-take_a_number(katz_deli, "Fyvish")
-
-def now_serving(line)
-  if line.length == 0
-    puts"There is nobody waiting to be served!"
+def now_serving(array)
+  if array.count == 0
+     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}."
-    line.shift
+     puts "Currently serving #{array.shift}."
   end
 end
-
-puts now_serving(katz_deli)
-puts katz_deli
